b4939b5940bf676b88298f7ed6b9ebdfb2586a08
David Oramas <darhat265@gmail.com>
2022-08-07 19:56:27 -0500

    changes in config webpack, babel and createroot

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import App from './Components/App'
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<h1>Hola Mundo</h1>, document.getElementById('root'))
+//ReactDOM.render(<App/>, document.getElementById('root'))
+
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,56 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 
 module.exports ={
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname,'public')
+    //mode: 'development', ESTO SOLO ES SI NO TENEMOS UN DEVSERVER EL CUAL SIRVE PARA UN LIVE RELOAD.
+    entry: './src/index.js', //por donde entra al correr
+    output: { //por donde sale xD
+        filename: 'js/[name].js', //Para que webpack genere un nombre diferente para cada vez que se contruya un bundle.js
+        path: path.resolve(__dirname,'public'),//__dirname toma la direccion del archivo
+        //resolve une el __dirname con public: 
+        //C:\Users\david.oramas\OneDrive - Netlogistik\Documentos\BaufestTennis\Baufest-Tenni\public
+        //como path.join, pero resolve se detiene al encontrar un delimitador,
+        //delimitador: windows: "/";  linux: "\"
+        publicPath: 'http://localhost:3000', //donde pondra todo el choro tus bundles.
     },
+  devServer: {
+    historyApiFallback: true, //Cuando hay una pagina no encontrada en los archivos y no tirar un 404
+    //recurre a alguno de los archivos en el historial para sustituir el error, react-router es ideal. 
+    compress: true,
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], //webpack no sabe que es jsx
+  },
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
         module: {
           rules: [
             {
-              test: /\.m?js$/,
+              test: /\.(js|jsx)$/,
               exclude: /node_modules/,
               use: {
-                loader: "babel-loader",
-                options: {
-                  presets: ['@babel/preset-env', '@babel/preset-react']
-                }
-              }
-            }
+                loader: 'babel-loader',
+              },
+            },
+            {
+              test: /\.html$/,
+              use: [
+                {
+                  loader: 'html-loader',
+                },
+              ],
+            },
           ]
-        }
+        },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: './index.html',
+    }),
+  ],
 };
